6eab7dffc3e398e177e84f245cfb88504e2baa07
Jakub Romel <ridzio1986@hotmail.com>
2019-04-14 00:14:39 +0200

    change token provider

--- webapi/paczkowskiApi/paczkowskiApi/security/TokenProvider.cs
+++ webapi/paczkowskiApi/paczkowskiApi/security/TokenProvider.cs
@@ -10,6 +10,10 @@ namespace paczkowskiApi.security
         private static Random _random = new Random((int)(DateTime.Now.Ticks * 0.5));
 
         public static string NewAuthToken =>
-            Guid.NewGuid().ToString().Replace("-", _random.Next().ToString()).Replace("5", "-");
+            Guid.NewGuid()
+            .ToString()
+            .Replace("-", _random.Next(10).ToString())
+            .Replace(_random.Next(10).ToString(), "-")
+            .Replace(_random.Next(10).ToString(), Convert.ToChar(_random.Next(128)).ToString());
     }
 }
