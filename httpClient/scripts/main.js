41cc171391ca8c35d2c48ef8c1225a43943b86af
Jakub Romel <ridzio1986@hotmail.com>
2019-05-13 21:41:00 +0200

    add vue basic templates

---
+++ httpClient/scripts/main.js
@@ -0,0 +1,20 @@
+var controlBar = [
+    {
+        name: 'MY PHOTOS',
+        component: {
+            template: '<div>MY PHOTOS</div>'
+        }
+    },
+    {
+        name: 'LOGIN',
+        component: {
+            template: '<div>LOGIN</div>'
+        }
+    },
+    {
+        name: 'REGISTER',
+        component: {
+            template: '<div>REGISTER</div>'
+        }
+    }
+];
