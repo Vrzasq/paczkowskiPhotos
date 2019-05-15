8cd8570891db6f819b2788ed3d3036e205a60097
Jakub Romel <ridzio1986@hotmail.com>
2019-05-16 01:55:18 +0200

    addc cors

--- webapi/paczkowskiApi/paczkowskiApi/Startup.cs
+++ webapi/paczkowskiApi/paczkowskiApi/Startup.cs
@@ -23,6 +23,12 @@ namespace paczkowskiApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            options.AddPolicy("default", builder =>
+                            builder
+                            .AllowAnyOrigin()
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddAuthentication(options =>
             {
@@ -45,6 +51,7 @@ namespace paczkowskiApi
                 app.UseHsts();
             }
 
+            app.UseCors("default");
             app.UseHttpsRedirection();
             app.UseMvc();
         }
