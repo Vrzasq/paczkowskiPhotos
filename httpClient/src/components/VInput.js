5c59fab2f141593993aae6396fcecdaf51f93444
Jakub Romel <ridzio1986@hotmail.com>
2019-05-16 17:34:43 +0200

    correct binding

--- httpClient/src/components/VInput.js
+++ httpClient/src/components/VInput.js
@@ -2,12 +2,15 @@ export default {
     props: [
         'label',
         'inputType',
-        'placeholder'
+        'placeholder',
+        'value'
     ],
     template: `
     <div class="form-row">
         <label class="w3-text-grey">{{ label }}</label>
-        <input class="w3-input w3-border" @input="$emit('input', $event.target.value)"
+        <input class="w3-input w3-border"
+            :value="value"
+            @input="$emit('input', $event.target.value)"
             :type="inputType"
             :placeholder="placeholder" />
     </div>
