74934036318b54eb9106bf0502b7eb67e510cd04
DIR\jakub.romel <jakub.romel@accenture.com>
2019-05-20 16:15:33 +0200

    add key binding for basic form

--- httpClient/src/components/VForm.js
+++ httpClient/src/components/VForm.js
@@ -24,9 +24,10 @@ export default {
     template: `
     <div class="form-data">
         <h2 class="w3-blue-grey" v-once>{{ title }}</h2>
-        <base-input v-for="formInput in formInputs"
+        <base-input v-for="(formInput, name) in formInputs"
             v-bind="formInput"
             v-model="formInput.value"
+            :key="name"
         ></base-input>
         <button class="w3-btn w3-blue-grey" @click="$emit('submit')" type="button">{{ buttonLabel }}</button>
     </div>
