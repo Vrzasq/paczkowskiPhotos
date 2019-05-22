8432cf15bf985433f2ddc4c1f14c6917e4a74dbd
Jakub Romel <ridzio1986@hotmail.com>
2019-05-22 23:37:55 +0200

    add categories for client

--- httpClient/src/components/PhotosComponent.js
+++ httpClient/src/components/PhotosComponent.js
@@ -9,7 +9,15 @@ export default {
     template: `
     <div class="photo-display">
         <h2 class="w3-blue-grey" v-once>{{ title }}</h2>
+        <div class="categories-container">
+            <h3 class="w3-blue-grey">{{ categoriesTitle }}</h3>
+            <div class="category" v-for="category in categories">
+                <span>{{ category.name }}</span>
+                <img @click="showCategory(category.name)" src="images/folder.png" />
+            </div>
+        </div>
         <div class="photos-container">
+            <h3 class="w3-blue-grey">{{ unCategoriesTitle }}</h3>
             <photo-view-component v-for="image, index in images"
                 :image="image.image"
                 :imageName="image.displayName"
@@ -21,11 +29,13 @@ export default {
     </div>
     `,
 
-    data: function () {
+    data () {
         return {
             title: 'MY PHOTOS',
+            categoriesTitle: 'CATEGORIES',
+            unCategoriesTitle: 'NOT CATEGORIZED',
             images: [],
-            dataType: 'data:image/jpeg;base64,'
+            categories: []
         };
     },
 
@@ -46,20 +56,44 @@ export default {
                 },
                 error: function (jqXHR, textStatus, errorThrown) { console.log(`${textStatus} ${errorThrown}`); }
             });
+        },
+
+        showCategory(category) {
+            //TODO add logic for switchich to category view
+            alert(category);
+        },
+
+        getCategories() {
+            let vm = this;
+            $.ajax({
+                method: 'GET',
+                xhrFields: { withCredentials: true },
+                url: Services.photo.getCategories,
+                success: function (data) {
+                    console.log(data);
+                    vm.categories = data;
+                },
+                error: function (jqXHR, textStatus, errorThrown) { console.log(`${textStatus} ${errorThrown}`); }
+            });
+        },
+
+        getPhotos() {
+            let vm = this;
+            $.ajax({
+                method: 'GET',
+                xhrFields: { withCredentials: true },
+                url: Services.photo.getUncategorizedPhotos,
+                success: function (data) {
+                    console.log(data);
+                    vm.images = data;
+                },
+                error: function (jqXHR, textStatus, errorThrown) { console.log(`${textStatus} ${errorThrown}`); }
+            });
         }
     },
 
     mounted: function () {
-        let vm = this;
-        $.ajax({
-            method: 'GET',
-            xhrFields: { withCredentials: true },
-            url: Services.photo.getPhotos,
-            success: function (data) {
-                console.log(data);
-                vm.images = data;
-            },
-            error: function (jqXHR, textStatus, errorThrown) { console.log(`${textStatus} ${errorThrown}`); }
-        });
+        this.getCategories();
+        this.getPhotos();
     },
 }
