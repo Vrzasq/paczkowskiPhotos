41dae07a263f8f8120f1f02914070944b439046b
Jakub Romel <ridzio1986@hotmail.com>
2019-05-23 23:37:08 +0200

    add category removal to client, add category route, some refactoring

--- httpClient/src/components/PhotosComponent.js
+++ httpClient/src/components/PhotosComponent.js
@@ -1,5 +1,4 @@
 import Services from '../services.js';
-import Commons from '../commons.js';
 import PhotoViewComponent from './PhotoViewComponent.js';
 
 export default {
@@ -22,22 +21,26 @@ export default {
     },
     template: `
     <div class="photo-display">
-        <h2 class="w3-blue-grey" v-once>{{ title }}</h2>
+        <h2 class="w3-blue-grey" v-once>{{ text.title }}</h2>
         <div class="categories-container" v-if="displayCategories">
-            <h3 class="w3-blue-grey">{{ categoriesTitle }}</h3>
-            <div class="category" v-for="category in categories">
+            <h3 class="w3-blue-grey">{{ text.categoriesTitle }}</h3>
+            <div class="category" v-for="category, index in categories">
                 <span>{{ category.name }}</span>
                 <img @click="showCategory(category.name)" src="images/folder.png" />
-                <button class="w3-btn w3-blue-grey">Edit</button>
-                <button class="w3-btn w3-blue-grey">Delete</button>
+                <button @click="editCategory(category.name, index)" class="w3-btn w3-blue-grey">{{ text.edit }}</button>
+                <button @click="deleteCategory(category.name, index)" class="w3-btn w3-blue-grey">{{ text.delete }}</button>
+            </div>
+            <div v-if="categories.length === 0">
+                <h4>{{ text.noCategories }}</h4>
             </div>
         </div>
         <div class="photos-container">
-            <h3 class="w3-blue-grey">{{ unCategoriesTitle }}</h3>
+            <h3 class="w3-blue-grey">{{ photosTitle }}</h3>
             <photo-view-component v-for="image, index in images"
                 :image="image.image"
                 :imageName="image.displayName"
                 :imageId="image.photoNum"
+                :category="image.category"
                 :key="image.photoNum"
                 @delete="deletePhoto($event, index)"
             ></photo-view-component>
@@ -47,12 +50,18 @@ export default {
 
     data () {
         return {
-            title: 'MY PHOTOS',
-            categoriesTitle: 'CATEGORIES',
-            unCategoriesTitle: 'NOT CATEGORIZED',
+            text: {
+                title: 'MY PHOTOS',
+                categoriesTitle: 'CATEGORIES',
+                unCategoriesTitle: 'NOT CATEGORIZED',
+                edit: 'Edit',
+                delete: 'Delete',
+                noCategories: 'NO CATEGORIES :(',
+                noImages: 'NO IMAGES :('
+            },
             images: [],
             categories: []
-        };
+        }
     },
 
     methods: {
@@ -70,13 +79,41 @@ export default {
                         vm.$delete(vm.images, index);
                     }
                 },
-                error: function (jqXHR, textStatus, errorThrown) { console.log(`${textStatus} ${errorThrown}`); }
+                error: function (jqXHR, textStatus, errorThrown) { console.log(`${jqXHR} ${textStatus} ${errorThrown}`); }
             });
         },
 
+        deleteCategory(category, index) {
+            let vm = this;
+            $.ajax({
+                method: 'DELETE',
+                contentType: 'application/json',
+                xhrFields: { withCredentials: true },
+                url: Services.photo.deleteCategory,
+                data: JSON.stringify({ name: category }),
+                success: function (data) {
+                    if (data) {
+                        alert('SUCCESS');
+                        vm.$delete(vm.categories, index);
+                    } else {
+                        alert('FAIL');
+                    }
+                },
+                error: function (jqXHR, textStatus, errorThrown) { console.log(`${jqXHR} ${textStatus} ${errorThrown}`); }
+            });
+        },
+
+        editCategory(category, index) {
+            alert(`${category} ${index}`)
+        },
+
         showCategory(category) {
-            //TODO add logic for switchich to category view
-            alert(category);
+            let data = {
+                isLoggedIn: true,
+                component: 'category',
+                eventData: { category }
+            }
+            this.$emit('component-change', data);
         },
 
         getCategories() {
@@ -89,7 +126,7 @@ export default {
                     console.log(data);
                     vm.categories = data;
                 },
-                error: function (jqXHR, textStatus, errorThrown) { console.log(`${textStatus} ${errorThrown}`); }
+                error: function (jqXHR, textStatus, errorThrown) { console.log(`${jqXHR} ${textStatus} ${errorThrown}`); }
             });
         },
 
@@ -104,7 +141,7 @@ export default {
                     console.log(data);
                     vm.images = data;
                 },
-                error: function (jqXHR, textStatus, errorThrown) { console.log(`${textStatus} ${errorThrown}`); }
+                error: function (jqXHR, textStatus, errorThrown) { console.log(`${jqXHR} ${textStatus} ${errorThrown}`); }
             });
         }
     },
@@ -114,6 +151,12 @@ export default {
             if (!this.category)
                 return Services.photo.getUncategorizedPhotos;
             return Services.photo.getPhotosForCategory + `/${this.category}`;
+        },
+
+        photosTitle() {
+            if (this.category !== '')
+                return this.category;
+            return this.text.unCategoriesTitle;
         }
     },
 
--- httpClient/src/index.js
+++ httpClient/src/index.js
@@ -2,46 +2,60 @@ import RegisterComponent from './components/RegisterComponent.js';
 import LoginComponent from './components/LoginComponent.js';
 import PhotosComponent from './components/PhotosComponent.js';
 import AddPhotoComponent from './components/AddPhotoComponent.js';
+import CategoryComponent from './components/CategoryComponent.js';
 
 let ControlBar = {
     photos: {
         name: 'MY PHOTOS',
         display: true,
-        component: PhotosComponent
+        component: PhotosComponent,
+        eventData: {}
     },
     addPhoto: {
         name: "ADD PHOTO",
         display: true,
-        component: AddPhotoComponent
+        component: AddPhotoComponent,
+        eventData: {}
     },
     register: {
         name: 'REGISTER',
         display: true,
-        component: RegisterComponent
+        component: RegisterComponent,
+        eventData: {}
     },
     login: {
         name: 'LOGIN',
         display: true,
-        component: LoginComponent
+        component: LoginComponent,
+        eventData: {}
     }
 }
 
 let RoutesComponents = {
     error: {
         name: 'ERROR',
-        component: ''
+        component: '',
+        eventData: {}
     },
     registerSuccess: {
         name: "REGISTER SUCCESS",
-        component: ''
+        component: '',
+        eventData: {}
     },
     photos: {
         name: 'MY PHOTOS',
-        component: PhotosComponent
+        component: PhotosComponent,
+        eventData: {}
     },
     login: {
         name: 'LOGIN',
-        component: LoginComponent
+        component: LoginComponent,
+        eventData: {}
+    },
+    category: {
+        name: 'CATEGORY',
+        component: CategoryComponent,
+        eventData: {}
     }
 }
 
